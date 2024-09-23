e72b956a452be5a2faaa4ee79e366d0ccdaeca8d
shedko <v.shedko@kpedev.by>
2024-09-23 12:56:05 +0300

    [FEATURE] Added products handlers

--- src/app/api/products/handler.py
+++ src/app/api/products/handler.py
@@ -1,4 +1,5 @@
-from app.api.products.schemas import ProductResponse
+from fastapi import Depends, HTTPException, Response, status
+from app.api.products.schemas import ProductResponse, ProductRequest
 
 
 async def get() -> ProductResponse:
@@ -9,3 +10,46 @@ async def get() -> ProductResponse:
         price=1000,
         count=1,
     )
+
+
+async def get_by_id(
+    product_id: int,
+) -> ProductResponse:
+    return ProductResponse(
+        id=1,
+        name='test name',
+        description='test description',
+        price=1000,
+        count=1,
+    )
+
+
+async def post(
+    request: ProductRequest,
+) -> ProductResponse:
+    return ProductResponse(
+        id=1,
+        name=request.name,
+        description=request.description,
+        price=request.price,
+        count=request.count,
+    )
+
+
+async def put(
+    product_id: int,
+    request: ProductRequest,
+) -> ProductResponse:
+    return ProductResponse(
+        id=product_id,
+        name=request.name,
+        description=request.description,
+        price=request.price,
+        count=request.count,
+    )
+
+
+async def delete(
+    product_id: int,
+) -> Response:
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/app/api/routes.py
+++ src/app/api/routes.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, FastAPI
 
-from app.api.products.handler import get as get_all_products
+from app.api.products.handler import (
+    get as get_all_products,
+    get_by_id as get_product_by_id,
+    post as create_new_product,
+    put as update_product_by_id,
+    delete as delete_product_by_id,
+)
 from app.api.products.schemas import ProductResponse
 
 
@@ -12,5 +18,29 @@ def setup_routes(app: FastAPI):
         methods=['GET'],
         response_model=ProductResponse,
     )(get_all_products)
+
+    api_products_router.api_route(
+        path='/{product_id}',
+        methods=['GET'],
+        response_model=ProductResponse,
+    )(get_product_by_id)
+
+    api_products_router.api_route(
+        path='/',
+        methods=['POST'],
+        response_model=ProductResponse,
+    )(create_new_product)
+
+    api_products_router.api_route(
+        path='/{product_id}',
+        methods=['PUT'],
+        response_model=ProductResponse,
+    )(update_product_by_id)
+
+    api_products_router.api_route(
+        path='/{product_id}',
+        methods=['DELETE'],
+        response_model=ProductResponse,
+    )(delete_product_by_id)
 
     app.include_router(api_products_router)
